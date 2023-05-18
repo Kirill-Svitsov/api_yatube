068b8e72ec37ea30cd916bc14b000096cfaf2ca4
Kirill Svitsov <blackaquitance@gmail.com>
2023-05-18 21:04:02 +0100

    First review version

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import get_object_or_404
-from posts.models import Comment, Group, Post, User
+from posts.models import Group, Post, User
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 
 from .premission import IsOwnerOrReadOnly
-from .serialazers import *
+from .serialazers import PostSerializer, UserSerializer, \
+    GroupSerializer, CommentSerializer
 
 
 class PostViewSet(viewsets.ModelViewSet):
